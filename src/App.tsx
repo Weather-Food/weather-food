863af9df05bd43ba17dd14366a74a18a2cedec94
JeongminJamie <sksrhcalsu@gmail.com>
2025-01-06 17:00:38 +0900

    Setup: add Routes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      첫 화면
-    </div>
-  );
-}
+const Home: React.FC = () => <h2>Home Page</h2>;
+const About: React.FC = () => <h2>About Page</h2>;
+
+const App: React.FC = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </div>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
