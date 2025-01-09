27a1456e40ce2c29d7f52725bc66c3a5423442b0
JeongminJamie <sksrhcalsu@gmail.com>
2025-01-09 18:17:42 +0900

    Feat: add ScrollTop component in App.tsx for page move

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Header, Footer } from "./components";
+import ScrollTop from "./components/ScrollTop";
 
 const App = () => (
   <>
+    <ScrollTop />
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
