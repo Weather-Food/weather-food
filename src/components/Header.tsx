cb0968916192928a0aa75c12ec38b05b63ef71a5
JeongminJamie <sksrhcalsu@gmail.com>
2025-01-09 16:43:46 +0900

    Design: add font for korean and english and delete unused test files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import "../scss/Header.scss";
+import "../styles/Header.scss";
 
 const Header = () => {
   const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
