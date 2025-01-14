9622dcb827e89ebdff5883c04997bd358633d0ba
JeongminJamie <sksrhcalsu@gmail.com>
2025-01-14 18:12:31 +0900

    Design: apply media query for mobile on main page including header, footer, weatherSection, recommendation components

--- src/components/Home/Recommendation.tsx
+++ src/components/Home/Recommendation.tsx
@@ -4,14 +4,18 @@ const Recommendation = () => {
   return (
     <div className="recommendation-container">
       <div className="recommend-statement--container">
-        <p className="statement">지금 날씨에 어울리는 음식은?</p>
+        <p className="statement">
+          <img src="./Home/food.svg" alt="food" />
+          지금 날씨에 딱인 음식은?
+        </p>
       </div>
       <div className="recommended-food--container">
+        {/* To-do: 날씨 추천 음식 패치 후 리스트 나열  */}
         <p>마라탕</p>
       </div>
       <div className="more-food--container">
         <p>추천 음식 더보기</p>
-        <img src="./Home/more.svg" alt="more" />
+        <img className="more-icon" src="./Home/more.svg" alt="more" />
       </div>
     </div>
   );
--- src/components/Home/WeatherSection.tsx
+++ src/components/Home/WeatherSection.tsx
@@ -23,15 +23,19 @@ const WeatherSection = () => {
       <div className="condition-container">
         <div className="hpa-container">
           {/* 여기 hpa 정보는 다른 걸로 대체 예정 */}
-          <img src="./Home/wind.svg" alt="hpa" />
+          <img className="hpa-icon" src="./Home/wind.svg" alt="hpa" />
           <p>720hpa</p>
         </div>
         <div className="pricipitation-container">
-          <img src="./Home/raindrop.svg" alt="pricipitation" />
+          <img
+            className="pricipitation-icon"
+            src="./Home/raindrop.svg"
+            alt="pricipitation"
+          />
           <p>32%</p>
         </div>
         <div className="wind-container">
-          <img src="./Home/wind.svg" alt="wind" />
+          <img className="wind-icon" src="./Home/wind.svg" alt="wind" />
           <p>12km/h</p>
         </div>
       </div>
