3e30df58bd977b9dbc418e1b13aad90e7f03c924
JeongminJamie <sksrhcalsu@gmail.com>
2025-02-03 18:19:44 +0900

    Refactor: change Router with createBrowserRouter creating router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import { Header, Footer } from "./components/common";
-import Hourly from "./pages/hourly/Hourly";
+import { Outlet } from "react-router-dom";
 import ScrollTop from "./components/common/ScrollTop";
-import ShowMoreFood from "./pages/ShowMoreFood"
-
-// createBrowserRouter를 이용한 라우팅으로 수정 필요!
+import { Header, Footer } from "./components/common";
 
 const App = () => (
-  <div className="app-container">
+  <div>
     <ScrollTop />
     <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hourly" element={<Hourly />} />
-      <Route path="/more-food" element={<ShowMoreFood />} />
-    </Routes>
+    <Outlet />
     <Footer />
   </div>
 );
--- src/components/ShowMoreFood/RecommendStatement.tsx
+++ src/components/ShowMoreFood/RecommendStatement.tsx
@@ -1,9 +1,11 @@
 import styles from "../../styles/ShowMoreFood/RecommendStatement.module.scss";
 
+import { FaLightbulb } from "react-icons/fa";
+
 const RecommendStatement = () => {
   return (
     <div className={styles.recommendStatement__container}>
-      <img className={styles.bulbIcon} />
+      <FaLightbulb className={styles.bulbIcon} />
       <p className={styles.recommendStatement}>
         흐린 날씨에는 따뜻한 국물 요리가 좋습니다. 추천 메뉴를 확인해보세요!
       </p>
